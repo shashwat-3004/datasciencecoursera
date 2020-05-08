fe98a2bb9ed93611e333e6c73261979fddac6052
Shashwat Patel <62668967+shashwat-3004@users.noreply.github.com>
2020-05-09 01:38:34 +0530

    Update cor-relation function.r

--- cor-relation function.r
+++ cor-relation function.r
@@ -1,31 +1,4 @@
-complete<- function(directory,id=1:332){
-        nob<-list()
-        idnum<-list()
-        wd<-getwd()
-        for (val in id){
 
-            if (val<10){
-                 id_value<-paste("0","0",val,sep="")
-            }
-            if (val>=10 & val<100){
-                 id_value<-paste("0",val,sep="")
-            }
-            if (val>=100){
-                 id_value<-val
-            }
-            file_name<-paste(wd,"/",directory,"/",id_value,".csv",sep="")
-            file_read<-read.csv(file_name,header=TRUE)
-
-            ans<-length(which(is.na(file_read["nitrate"])==FALSE &is.na(file_read["sulfate"])==FALSE))
-
-            nob<-append(nob,ans)
-            idnum<-append(idnum,val)
-        }
-        final<-matrix(c(idnum,nob),ncol=2)
-        colnames(final)<-c("id","nobs")
-        rownames(final)<-c(1:length(idnum))
-        return(final)
-}
 cor_value<-function(directory, threshold){
   wd<-getwd()
   id=1:332
